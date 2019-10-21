9a80cdad1c17b8184d26f52f2de2969c7bec42b2
ijuhom <2014brand@naver.com>
2019-10-21 13:09:30 +0900

    계산서 의 여러버전을 만들수있게끔 리팩토링 하였음.

--- src/Video/Cuustomer/MoveMethod/Refactoring/Customer.java
+++ src/Video/Cuustomer/MoveMethod/Refactoring/Customer.java
@@ -25,11 +25,10 @@ public class Customer {
 	 * 1. 수정되지 않는 변수는 파라미터로 넘길 수 있다.
 	 * 2. 논리적으로 연관있는 코드를 찾아서 Extract Method(메소드 추출) 한다  
 	 * 3. 현재 클래스의 정보를 사용하지 않는 메소드는 적절한 쓰임이있는 클래스로 Move Method(메서드 이동) 한다.
+	 * 4. 임시변수 totalAmount와 frequentRenterPoints 를 (query method) 로 변경 
 	 * */
 	
 	public String statement() {
-		double totalAmout = 0;
-		int frequentRenterPoints = 0;
 		Iterator<Rental> rentalElements = rentals.iterator();
 		String result = "Rental Record for" + getName() + "\n";
 
@@ -37,19 +36,57 @@ public class Customer {
 		while (rentalElements.hasNext()) {
 			Rental each = rentalElements.next();
 
-			frequentRenterPoints += each.getFrequentRenterPoints();
-
 			// 요금 계산 결과
 			result += "\t" + each.getMovie().geTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmout += each.getCharge();
 		}
 		// 푸터 추가
-		result += "Amount owed is" + String.valueOf(totalAmout) + "\n"; // 총가격
-		result += "You earned" + String.valueOf(frequentRenterPoints) + 
+		result += "Amount owed is" + String.valueOf(getTotalCharge()) + "\n"; // 총가격
+		result += "You earned" + String.valueOf(getTotalFrequentRenterPoints()) + 
 				  "frequentRenterPoints";// 마일리지
 
 		return result;
+	}
+	//html용계산서 ... 어떠한 종류의 계산서가와도 빠르고 쉽게 만들수있음
+	public String htmlStatement() {
+		Iterator<Rental> rentalElements = rentals.iterator();
+		String result = "<h1>Rental Record for</h1>" + getName() + "<br>\n";
 
+		//각 영화에 대한 요금 결정
+		while (rentalElements.hasNext()) {
+			Rental each = rentalElements.next();
+
+			// 요금 계산 결과
+			result += "\t" + each.getMovie().geTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+		}
+		// 푸터 추가
+		result += "<p>Amount owed is" + String.valueOf(getTotalCharge()) + "\n"; // 총가격
+		result += "You earned" + String.valueOf(getTotalFrequentRenterPoints()) + 
+				  "frequentRenterPoints</p>";// 마일리지
+
+		return result;
+	}
+	
+	//frequentRenterPoints 를 메소드로 변경
+	public int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Iterator<Rental> rentalElements = rentals.iterator();
+		while (rentalElements.hasNext()) {
+			Rental each = rentalElements.next();
+			result += each.getFrequentRenterPoints();
+			
+		}
+		return result;
+	}
+	//totalAmount 를 메소드로 변경
+	public double getTotalCharge() {
+		double result = 0;
+		Iterator<Rental> rentalElements = rentals.iterator();
+		while (rentalElements.hasNext()) {
+			Rental each = rentalElements.next();
+			result += each.getCharge();
+			
+		}
+		return result;
 	}
 
 
