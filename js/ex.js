23e6941b134a195da88174b1065c0a5e9870960d
ung <2014brand@naver.com>
2021-05-14 13:35:34 +0900

    리팩터링 예시

--- js/ex.js
+++ js/ex.js
@@ -1,1 +1,141 @@
-console.log(`hello`);
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalcylator(aPerformance, aPlay);
+    default:
+      throw new Error(`알수없는 장르; ${aPlay.type}`);
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+class ComedyCalcylator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+
+  return result;
+
+  function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
+    const result = Object.assign({}, aPerformance);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function totalVolumeCredits(statementData) {
+    return statementData.performances.reduce(
+      (total, c) => c.volumeCredits + total,
+      0
+    );
+  }
+
+  function totalAmount(statementData) {
+    return statementData.performances.reduce((total, c) => c.amount + total, 0);
+  }
+}
+
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function renderHtml(data) {
+  let result = `<h1><청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+
+  for (let perf of data.performances) {
+    result += `<tr> <td>${perf.play.name}</td><td>(${
+      perf.audience
+    }석)</td><td> ${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += "</table>";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트 <em>${data.totalVolumeCredits}</em>점<p>\n`;
+  return result;
+}
+
+(async function () {
+  const invoices = await fetch("/js/invoices.json").then((v) => v.json());
+  const plays = await fetch("/js/plays.json").then((v) => v.json());
+
+  console.log(statement(...invoices, plays));
+  document.querySelector("body").innerHTML = htmlStatement(...invoices, plays);
+})();
