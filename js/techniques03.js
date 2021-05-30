a239048c72891d9986ce1da8166866fff3adda1f
ung <2014brand@naver.com>
2021-05-30 13:54:20 +0900

    변수 추출하기

---
+++ js/techniques03.js
@@ -0,0 +1,49 @@
+/**
+ * 변수 추출하기 <-> 변수 인라인하기
+ */
+
+function price(order) {
+  return (
+    order.quantity * order.itemPrice -
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  );
+}
+
+function price(order) {
+  const basePrice = order.quantity * order.itemPrice;
+  const quantitiyDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantitiyDiscount + shipping;
+}
+
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return this.basePrice - this.quantitiyDiscount + this.shipping;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get quantitiyDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
+}
